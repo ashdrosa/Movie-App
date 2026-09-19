3166c1b94a9ff1af18780d1f64b0601fb51437c2
Ashley Dela Rosa <ashdrosa@gmail.com>
2026-09-19 13:36:28 -0400

    created register placeholder

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,4 +10,8 @@ class MainController {
     function login() {
         echo \Template::instance()->render('login.html');
     }
+
+    function register() {
+        echo \Template::instance()->render('register.html');
+    }
  }
---
+++ tmp/1rhmuqyegwrps.187tmtf99q0kx.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div>
+      <label>First Name:</label>
+      <input type="text" id="first-name" placeholder="First Name" />
+      <br />
+      <label>Birthday:</label>
+      <input type="date" id="DOB" />
+      <br />
+      <label>Email:</label>
+      <input type="email" id="email" placeholder="email" />
+      <br />
+      <label>Username:</label>
+      <input type="text" id="username" placeholder="username" />
+      <br />
+      <label>Password:</label>
+      <input type="password" id="pass" placeholder="password" />
+      <br />
+      <br />
+
+      <button id="register">Submit</button>
+    </div>
+  </body>
+</html>
